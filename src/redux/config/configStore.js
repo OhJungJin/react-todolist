9af7c19c9834ff837fe20f90f8089ab5ba1aec40
JUNGJIN <ojj824@naver.com>
2022-08-20 15:53:31 +0900

    feat: redux-toolkit 설정

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,5 +1,20 @@
-import { createStore } from "redux";
+import { legacy_createStore as createStore } from "redux";
+import { createAction } from "@reduxjs/toolkit";
 import { combineReducers } from "redux";
+
+const addToDo = createAction("ADD");
+const deleteToDo = createAction("DELETE");
+
+const reducer = (state = [], action) => {
+	switch (action.type) {
+		case addToDo.type:
+			return [{ text: action.text, id: Date.now() }, ...state];
+		case deleteToDo.type:
+			return state.filter((toDo) => toDo.id !== action.id);
+		default:
+			return state;
+	}
+};
 
 const rootReducer = combineReducers({});
 const store = createStore(rootReducer);
